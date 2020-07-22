f4d0069e6e33ef515990f2e0e30ac55c8d3e0907
samar-1601 <samar.pp.singh@gmail.com>
2020-07-22 16:57:45 +0530

    Cart Functionality added

--- shop/views.py
+++ shop/views.py
@@ -4,11 +4,25 @@ from .models import Product
 from math import ceil
 
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    params = {'no_of_slides':nSlides, 'range': range(nSlides), 'product': products}
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    categories = {item['category'] for item in catprods} #assigns category of items in catprods to cats
+    # a list of category is made
+    for category in categories:
+        prod = Product.objects.filter(category=category) 
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
 
 def about (request):
